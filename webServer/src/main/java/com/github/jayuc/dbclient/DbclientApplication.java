54a179a79499e2cf7c942253f37b4c2f363e5138
余杰 <yujie@ahsoft.com>
2019-04-03 17:34:17 +0800

    初始化web server项目

---
+++ webServer/src/main/java/com/github/jayuc/dbclient/DbclientApplication.java
@@ -0,0 +1,13 @@
+package com.github.jayuc.dbclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbclientApplication.class, args);
+	}
+
+}
